15d64d8c6092be6cf95dcf3f34282eb4ea555b02
Ss Aaa <ssaaa.ele@gmail.com>
2024-05-14 21:09:13 +0200

    docker compose works, but mariadb script has some problem

--- inception/srcs/requirements/mariadb/conf/script.sh
+++ inception/srcs/requirements/mariadb/conf/script.sh
@@ -1,5 +1,33 @@
-mysql -e "CREATE DATABASE SQL_DATABASE";
-mysql -e "CREATE USER SQL_USER IDENTIFIED BY SQL_PASSWORD";
-mysql -e "GRANT ALL PRIVILEGES ON SQL_DATABASE.* TO SQL_USER IDENTIFIED BY SQL_PASSWORD";
-mysql -e "FLUSH PRIVILEGES";
-#exit
+#!/bin/bash
+
+service mariadb start \
+    && until mysqladmin ping &>/dev/null; do \
+           sleep 1; \
+       done
+
+mariadb -u root -e "CREATE DATABASE $SQL_DATABASE;
+                    CREATE USER $SQL_USER IDENTIFIED BY $SQL_PASSWORD;
+                    GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER IDENTIFIED BY $SQL_PASSWORD;
+                    ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_PASSWORD;
+                    FLUSH PRIVILEGES;"
+
+exec "$@"
+
+# mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+# set -e
+
+# if [ -z "$(ls -A /var/lib/mysql)" ]; then
+#     echo "Initialisation de la base de données..."
+
+#     until mysqladmin ping >/dev/null 2>/dev/null; do
+#         sleep 1
+#     done
+
+#     mariadb -u root -e "CREATE DATABASE $SQL_DATABASE;
+#                         CREATE USER $SQL_USER IDENTIFIED BY $SQL_PASSWORD;
+#                         GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER IDENTIFIED BY $SQL_PASSWORD;
+#                         ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_PASSWORD;
+#                         FLUSH PRIVILEGES;"
+
+# fi
+
