c6ad1a84187bfad467006022cdb4e645d4050c6b
root <root@inception.inception.org>
2024-09-21 10:36:03 +0200

    error on wp cli

--- inception/srcs/requirements/mariadb/conf/script.sh
+++ inception/srcs/requirements/mariadb/conf/script.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-mysqld_safe &
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
+mysqld_safe --nowatch &
 
 until mysqladmin ping &>/dev/null; do \
        sleep 1; \
 done
 
+echo "Creation of Database"
+
 mariadb -u root -e "CREATE DATABASE $DATABASE;
-                    CREATE USER $USER IDENTIFIED BY $PASSWORD;
-                    GRANT ALL PRIVILEGES ON $DATABASE.* TO $USER IDENTIFIED BY $PASSWORD;
+                    CREATE USER $USER IDENTIFIED BY '$PASSWORD';
+                    GRANT ALL PRIVILEGES ON $DATABASE.* TO $USER IDENTIFIED BY '$PASSWORD';
                     FLUSH PRIVILEGES;"
 
-mysqladmin -u root shutdown
+echo "Database Created"
 
-exec mysqld_safe
+while true; do
+    sleep 1
+done
--- inception/srcs/requirements/wordpress/conf/config.sh
+++ inception/srcs/requirements/wordpress/conf/config.sh
@@ -17,12 +17,20 @@ echo "PASSWORD2: $PASSWORD"
 echo "DATABASE2: $DATABASE"
 
 if ! test -f wp-config.php; then
-    wp config create --dbname=$NAME --dbuser=$USER --dbpass=$PASSWORD --dbhost=mariadb:3306
+    wp config create --allow-root --dbname=$NAME --dbuser=$USER --dbpass=$PASSWORD --dbhost=mariadb:3306 --path=/var/www/wordpress
 fi
 
+echo "USER3: $USER"
+echo "PASSWORD3: $PASSWORD"
+echo "DATABASE3: $DATABASE"
+
 if ! wp core is-installed; then
-    wp core install --title=$TITLE --url=svan-de-.42.fr --admin_user=$ADMIN_USERNAME --admin_email=$ADMIN_USERNAME@worpress.com --admin_password=$ADMIN_PASSWORD
+    wp core install --allow-root --title=$TITLE --url=svan-de-.42.fr --admin_user=$ADMIN_USERNAME --admin_email=$ADMIN_USERNAME@worpress.com --admin_password=$ADMIN_PASSWORD
     wp user create $USER_USERNAME $USER_USERNAME@wordpress.com --user_pass=$USER_PASSWORD
 fi
+
+echo "USER4: $USER"
+echo "PASSWORD4: $PASSWORD"
+echo "DATABASE4: $DATABASE"
 
 exec php-fpm81 -F
