27e91358b47a28ef1837a6f65c7980296cd38367
root <root@inception.inception.org>
2024-09-24 12:58:19 +0200

    site is accessible, problem with https and users connexion

--- inception/srcs/requirements/mariadb/conf/script.sh
+++ inception/srcs/requirements/mariadb/conf/script.sh
@@ -8,14 +8,10 @@ until mysqladmin ping &>/dev/null; do \
        sleep 1; \
 done
 
-echo "Creation of Database"
-
 mariadb -u root -e "CREATE DATABASE $DATABASE;
                     CREATE USER $USER IDENTIFIED BY '$PASSWORD';
                     GRANT ALL PRIVILEGES ON $DATABASE.* TO $USER IDENTIFIED BY '$PASSWORD';
                     FLUSH PRIVILEGES;"
-
-echo "Database Created"
 
 while true; do
     sleep 1
--- inception/srcs/requirements/wordpress/conf/config.sh
+++ inception/srcs/requirements/wordpress/conf/config.sh
@@ -2,44 +2,21 @@
 
 set -e
 
-cd /var/www && tar -xf wordpress-6.5-fr_FR.tar.gz
+cd /var/www && tar -xf wordpress-6.5-fr_FR.tar.gz -C /var/www/html --strip-components=1
 
-cd /var/www/wordpress
-
-echo "USER: $USER"
-echo "PASSWORD: $PASSWORD"
-echo "DATABASE: $DATABASE"
+cd /var/www/html
 
 while ! mariadb -h mariadb -u $USER -p$PASSWORD -P 3306 $DATABASE -e ";" 2>/dev/null ; do
 	sleep 1
 done
 
-echo "USER2: $USER"
-echo "PASSWORD2: $PASSWORD"
-echo "DATABASE2: $DATABASE"
-
 if ! test -f wp-config.php; then
-    wp config create --allow-root --dbname=$DATABASE --dbuser=$USER --dbpass=$PASSWORD --dbhost=mariadb:3306 --path=/var/www/wordpress
+    wp config create --allow-root --dbname=$DATABASE --dbuser=$USER --dbpass=$PASSWORD --dbhost=mariadb:3306 --path=/var/www/html
 fi
-
-echo "USER3: $USER"
-echo "PASSWORD3: $PASSWORD"
-echo "DATABASE3: $DATABASE"
 
 if ! wp core is-installed --allow-root ; then
     wp core install --allow-root --title=$TITLE --url=svan-de-.42.fr --admin_user=$ADMIN_USERNAME --admin_email=$ADMIN_USERNAME@worpress.com --admin_password=$ADMIN_PASSWORD
     wp user create --allow-root $USER_USERNAME $USER_USERNAME@wordpress.com --user_pass=$USER_PASSWORD
 fi
 
-echo "USER4: $USER"
-echo "PASSWORD4: $PASSWORD"
-echo "DATABASE4: $DATABASE"
-
-echo "fin 1"
-
 exec "$@"
-
-echo "fin 2"
-# php-fpm7.4 -F
-
-# echo "fin"
